b95dd6fcdb6c0e037d35874f839404a56cbc04ac
Liu Ladniak <liuladniak@gmail.com>
2024-06-04 22:48:26 -0400

    adds header styling

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -3,7 +3,9 @@ import logo from "../../assets/logo/BrainFlix-logo.svg";
 function Logo() {
   return (
     <div className="header__logo">
-      <img src={logo} alt="BrainFlix logo" />
+      <a href="/">
+        <img src={logo} alt="BrainFlix logo" />
+      </a>
     </div>
   );
 }
