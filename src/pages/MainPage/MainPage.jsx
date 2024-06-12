6d1772aaeb1db26a2d96aa85cfa6aca4089cd57d
Liu Ladniak <liuladniak@gmail.com>
2024-06-11 22:43:24 -0400

    initial set up of api endpoint, gets video data

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,4 +1,7 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+// import { useParams } from "react-router-dom";
+import { API_KEY, API_URL } from "../../utils/api";
 import videoDetailsData from "../../data/video-details.json";
 import Header from "../../components/Header/Header";
 import SelectedVideo from "../../components/SelectedVideo/SelectedVideo";
@@ -7,6 +10,29 @@ import VideoNav from "../../components/VideoNav/VideoNav";
 import "./MainPage.scss";
 
 function MainPage() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [videos, setVideos] = useState(null);
+  const [error, setIsError] = useState(false);
+  // const { videoId } = useParams();
+  // console.log(videoId);
+
+  const getVideos = async () => {
+    try {
+      const getVideoResponse = await axios.get(
+        `${API_URL}videos/?api_key=${API_KEY}`
+      );
+      console.log(getVideoResponse);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  useEffect(() => {
+    getVideos();
+  }, []);
+  // ********************************
+  // Prev solution:
+  // ********************************
+
   const [selectedVideo, setSelectedVideo] = useState(videoDetailsData[0]);
   const [nextVideos, setNextVideos] = useState(videoDetailsData.slice(1));
 
@@ -24,6 +50,8 @@ function MainPage() {
       setNextVideos(updatedNextVideos);
     }
   };
+  // ********************************
+
   return (
     <div className="app">
       <Header />
---
+++ src/utils/api.js
@@ -0,0 +1,2 @@
+export const API_URL = "https://unit-3-project-api-0a5620414506.herokuapp.com/";
+export const API_KEY = "3a8431e4-c2e5-48d5-846c-ab1e006825b2";
