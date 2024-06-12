3e8be0113325b4cc9b78669462a663abda7e01f2
Liu Ladniak <liuladniak@gmail.com>
2024-06-11 22:16:52 -0400

    fixes a route bug

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<MainPage />} />
+        <Route path="/" index element={<MainPage />} />
         <Route path="/videos/:videoId" element={<MainPage />} />
         <Route path="/upload" element={<UploadPage />} />
         <Route path="*" element={<PageNotFound />} />
